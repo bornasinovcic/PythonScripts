5fa9072722433267d5b4aa3fc053e3559455cb42
bornasinovcic <sinovcic.borna@gmail.com>
2022-07-03 01:50:55 +0200

    modified: README.md and fileRenaming.py

--- fileRenaming.py
+++ fileRenaming.py
@@ -1,40 +1,38 @@
 import os
-import operator
-
 
 
 def main():
     j = 1
-    rijecnik = {}
+    dictionary = {}
     extension = ".jpg"
-    os.chdir("../wallpapers")
+    os.chdir("../wallpapers-master")
     for i in os.listdir():
-        rijecnik.update({i:os.path.getsize(i)})
-    sorted_d = dict(sorted(rijecnik.items(), key = operator.itemgetter(1), reverse = True))
+        dictionary.update({i: os.path.getsize(i)})
+    sorted_d = sorted(dictionary, key=dictionary.get, reverse=True)
     my_list = []
-    for x, y in sorted_d.items():
+    for x in sorted_d:
         my_list.append(x)
     for i in my_list:
         if j <= 9:
-            if os.path.isfile("0000" + str(j) + extension):
-                j += 1
-                continue
-            else:
-                os.rename(i, "0000" + str(j) + extension)
-                j += 1
-        elif 10 <= j <= 99:
-            if os.path.isfile("000" + str(j) + extension):
-                j += 1
-                continue
-            else:
-                os.rename(i, "000" + str(j) + extension)
-                j += 1
-        elif 100 <= j <= 999:
             if os.path.isfile("00" + str(j) + extension):
                 j += 1
                 continue
             else:
                 os.rename(i, "00" + str(j) + extension)
+                j += 1
+        elif 10 <= j <= 99:
+            if os.path.isfile("0" + str(j) + extension):
+                j += 1
+                continue
+            else:
+                os.rename(i, "0" + str(j) + extension)
+                j += 1
+        elif 100 <= j <= 999:
+            if os.path.isfile("" + str(j) + extension):
+                j += 1
+                continue
+            else:
+                os.rename(i, "" + str(j) + extension)
                 j += 1
         elif 1000 <= j <= 9999:
             if os.path.isfile("0" + str(j) + extension):
@@ -50,7 +48,6 @@ def main():
             else:
                 os.rename(i, str(j) + extension)
                 j += 1
-
 
 
 if __name__ == "__main__":
