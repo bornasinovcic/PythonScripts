50ca27b05fe212d6ef7a4390cac4f33b5e3d9c4c
bornasinovcic <sinovcic.borna@gmail.com>
2022-07-03 16:21:08 +0200

    modified: fileRenaming.py

--- fileRenaming.py
+++ fileRenaming.py
@@ -9,30 +9,27 @@ def main():
     for i in os.listdir():
         dictionary.update({i: os.path.getsize(i)})
     sorted_d = sorted(dictionary, key=dictionary.get, reverse=True)
-    my_list = []
-    for x in sorted_d:
-        my_list.append(x)
-    for i in my_list:
+    for i in sorted_d:
         if j <= 9:
+            if os.path.isfile("0000" + str(j) + extension):
+                j += 1
+                continue
+            else:
+                os.rename(i, "0000" + str(j) + extension)
+                j += 1
+        elif 10 <= j <= 99:
+            if os.path.isfile("000" + str(j) + extension):
+                j += 1
+                continue
+            else:
+                os.rename(i, "000" + str(j) + extension)
+                j += 1
+        elif 100 <= j <= 999:
             if os.path.isfile("00" + str(j) + extension):
                 j += 1
                 continue
             else:
                 os.rename(i, "00" + str(j) + extension)
-                j += 1
-        elif 10 <= j <= 99:
-            if os.path.isfile("0" + str(j) + extension):
-                j += 1
-                continue
-            else:
-                os.rename(i, "0" + str(j) + extension)
-                j += 1
-        elif 100 <= j <= 999:
-            if os.path.isfile("" + str(j) + extension):
-                j += 1
-                continue
-            else:
-                os.rename(i, "" + str(j) + extension)
                 j += 1
         elif 1000 <= j <= 9999:
             if os.path.isfile("0" + str(j) + extension):
