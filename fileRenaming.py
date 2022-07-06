bc68517a60706a2805856888b5f5b68c58eafcd0
bornasinovcic <sinovcic.borna@gmail.com>
2022-07-06 11:40:27 +0200

    deleted: extensionChanging.py and fileRenaming.py

--- fileRenaming.py
+++ fileRenaming.py
@@ -5,41 +5,28 @@ def main():
     j = 1
     dictionary = {}
     extension = ".jpg"
-    os.chdir("../wallpapers-master")
+    os.chdir("../../Desktop/wallpapers-master")
     for i in os.listdir():
-        dictionary.update({i: os.path.getsize(i)})
+        if i.endswith(extension):
+            dictionary.update({i: os.path.getsize(i)})
     sorted_d = sorted(dictionary, key=dictionary.get, reverse=True)
     for i in sorted_d:
         if j <= 9:
-            if os.path.isfile("00" + str(j) + extension):
-                j += 1
-            else:
+            if not os.path.isfile("00" + str(j) + extension):
                 os.rename(i, "00" + str(j) + extension)
-                j += 1
         elif 10 <= j <= 99:
-            if os.path.isfile("0" + str(j) + extension):
-                j += 1
-            else:
+            if not os.path.isfile("0" + str(j) + extension):
                 os.rename(i, "0" + str(j) + extension)
-                j += 1
         elif 100 <= j <= 999:
-            if os.path.isfile("" + str(j) + extension):
-                j += 1
-            else:
+            if not os.path.isfile("" + str(j) + extension):
                 os.rename(i, "" + str(j) + extension)
-                j += 1
         elif 1000 <= j <= 9999:
-            if os.path.isfile("0" + str(j) + extension):
-                j += 1
-            else:
+            if not os.path.isfile("0" + str(j) + extension):
                 os.rename(i, "0" + str(j) + extension)
-                j += 1
         else:
-            if os.path.isfile(str(j) + extension):
-                j += 1
-            else:
+            if not os.path.isfile(str(j) + extension):
                 os.rename(i, str(j) + extension)
-                j += 1
+        j += 1
 
 
 if __name__ == "__main__":
